dea195effc198f41f7ee4886d693c69b37bd8de6
ozkan.ada <ozkanada86@gmail.com>
2014-04-16 00:18:59 +0300

    Country detail bean yapıldı.

---
+++ SportStats/src/com/javateam/sportstats/faces/CountryDetailBean.java
@@ -0,0 +1,10 @@
+package com.javateam.sportstats.faces;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class CountryDetailBean {
+	
+}
